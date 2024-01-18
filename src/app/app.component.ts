5e7c87ea035f1c1e07c45495ca9df35316daa37b
alvgm98 <alvgm1998@gmail.com>
2024-01-18 17:29:14 +0100

    Esqueleto de la pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ahorcado';
+  palabraIntroducida: string = '';
+  palabra: string = '';
+  intentos: number = 0;
+
+  acertada: boolean = false;
+
+
+  setPalabraIntroducida(palabraIntroducida: string) {
+    this.palabraIntroducida = palabraIntroducida;
+
+    // Rellena 'palabra' de '*' los cuales se usarán para controlar si el Usuario ha acertado la palabra.
+    this.palabra = this.palabra.padEnd(palabraIntroducida.length, '*');
+  }
+
+  comprobarLetra(letra: string) {
+
+    if (this.palabraIntroducida.includes(letra)) {
+      for (let i = 0; i < this.palabra.length; i++) {
+        // Introduce la letra acertada en 'palabra' en las posiciones correspondientes.
+        if (this.palabraIntroducida.charAt(i) == letra) {
+          this.palabra = this.palabra.substring(0, i) + letra + this.palabra.substring(i + 1);
+        }
+        // Comprueba si se han terminado los '*'
+        if (!this.palabra.includes('*')) {
+          this.acertada = true;
+        }
+      }
+
+    } else {
+      this.intentos++;
+    }
+
+  }
+
 }
